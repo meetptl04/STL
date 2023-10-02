156102753e656a8370d020284c5cb51ca735d64a
meetptl04 <meet06032004@gmail.com>
2023-10-02 23:00:00 +0530

    add

--- stl1.cpp
+++ stl1.cpp
@@ -1,7 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Pairs  // pairs --> inside utility library
+void explainPair(){
+
+    pair<int,int> p={1,3};
+
+    cout<< p.first <<" "<<p.second;
+
+    pair<int , pair<int ,int >> P={1,{3,4}};
+
+    cout<< P.first <<" "<<P.second.second <<" "<<P.second.first;
+
+    pair<int,int>arr[]={{1,2},{3,4},{5,6}};
+
+    cout<< arr[1].second;
+
+}
+
 int main(){
-    cout<<"Hello World!!";
+    cout<<"Hello World!!\n";
+    explainPair();
     return 0;
 }
